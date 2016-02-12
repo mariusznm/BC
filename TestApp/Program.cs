cc5c5b8d46ad38f3c96a8c21d196e832daf6041d
Mariusz Pecherski <mariusz.pecherski@gmail.com>
2016-02-12 17:09:12 +0100

    Updated time format in test app

--- TestApp/Program.cs
+++ TestApp/Program.cs
@@ -2,6 +2,7 @@
 
 namespace TestApp
 {
+    using System.Globalization;
     using System.Threading;
 
     using BerlinClock.Classes.Clock;
@@ -10,8 +11,8 @@ namespace TestApp
     {
         private static void Main(string[] args)
         {
-            var clock = new Clock(DateTime.Now.ToString("T"));
-            var clock2 = new Clock2(DateTime.Now.ToString("T"));
+            var clock = new Clock(DateTime.Now.ToString("HH:mm:ss"));
+            var clock2 = new Clock2(DateTime.Now.ToString("HH:mm:ss"));
             while (true)
             {
                 Console.Clear();
@@ -20,8 +21,8 @@ namespace TestApp
                 Console.WriteLine();
                 Console.Write(clock2.Display());
                 Thread.Sleep(100);
-                clock.SetTime(DateTime.Now.ToString("T"));
-                clock2.SetTime(DateTime.Now.ToString("T"));
+                clock.SetTime(DateTime.Now.ToString("HH:mm:ss"));
+                clock2.SetTime(DateTime.Now.ToString("HH:mm:ss"));
             }
         }
     }
